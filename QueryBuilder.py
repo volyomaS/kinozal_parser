b2b336944b754d95b996db44d3e7d4669f25e752
volyomaS <rastoden@gmail.com>
2021-03-22 00:00:44 +0300

    type annotations added

--- QueryBuilder.py
+++ QueryBuilder.py
@@ -1,17 +1,17 @@
 class QueryBuilder:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__query = "https://kinozal-tv.appspot.com/browse.php?"
         self.__name = "s="
         self.__q = ""
 
-    def add_name(self, film_name):
+    def add_name(self, film_name: str) -> None:
         self.__name += f"{film_name} "
 
-    def add_year(self, year):
+    def add_year(self, year: int) -> None:
         self.__q += f"d={year}&"
 
-    def add_quality(self, quality):
+    def add_quality(self, quality: str) -> None:
         self.__name += f"{quality} "
 
-    def build(self):
+    def build(self) -> str:
         return self.__query + self.__name.strip() + "&" + self.__q
